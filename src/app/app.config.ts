b57202b8717f69052694f2ab0cef332fe3815e97
Ruby020905 <rubyoforiwa@gmail.com>
2026-01-06 19:33:53 -0500

    integracion con keycload

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideKeycloak } from 'keycloak-angular';
+import { routes } from './app.routes';
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
+    // 1. Detección de cambios estándar (Obligatorio para Keycloak)
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    
+    provideKeycloak({
+      config: {
+        url: 'http://localhost:8082', 
+        realm: 'inventory',
+        clientId: 'angular-client'
+      },
+      initOptions: {
+        onLoad: 'login-required',
+        checkLoginIframe: false,
+        flow: "standard"
+        // He quitado silentCheckSsoRedirectUri para asegurar que primero cargue el login
+      }
+    })
   ]
 };
